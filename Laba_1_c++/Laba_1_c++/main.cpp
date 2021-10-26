ea80bf67c55c7ee568de0f6645dab891bf8db7a3
Артем Почетуха <93141700+PochetuhaArtem@users.noreply.github.com>
2021-10-26 22:19:24 +0300

    c++

---
+++ Laba_1_c++/Laba_1_c++/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+
+class MyFunc
+{
+  public:
+        void CinArguments()
+        {
+           cout<<"Введите свой вариант: "<<endl;
+           cin>>var;
+           cout<<"Начальное значение аргумента: "<<endl;
+           cin>>x1;
+           cout<<"Конечное значение аргумента: "<<endl;
+           cin>>x2;
+           cout<<"Количество точек(1-вариант) / Шаг аргумента(2-вариант)"<<endl;
+           cin>>N;
+        }
+       void Dump()
+       {
+          cout<<"\t| "<<i<<"|\t |"<<x1<<"|\t |"<<x3<<"|\t"<<endl;
+          cout<<"+----------+----------+----------+\n"<<endl;
+       }
+    void Set(int _x1,int _x2,int _N,int _var)
+    {
+         x1  = _x1;
+         x2  = _x2;
+         N   = _N;
+         var = _var;
+    }
+       void Get()
+       {
+          cout<<"variant: "<<var<<endl;
+          cout<<"x1: "<<x1<<endl;
+          cout<<"x2: "<<x2<<endl;
+          cout<<"N or delta: "<<N<<endl;
+       }
+    void Calc()
+    {
+        if(var==1)
+         {
+               cout<<"**********************************"<<endl;
+               cout<<"\t* N * X * F(X) *\t"<<endl;
+               cout<<"**********************************"<<endl;
+               cout<<"+----------+----------+----------+\n"<<endl;
+            for(i = 1 ; i<=N; i++)
+          {
+            x3=x1*2;
+            //my function F(x)=x1*2
+            Dump();
+            x1++;
+           }
+         }
+        else if(var==2)
+        {
+              cout<<"**********************************"<<endl;
+              cout<<"\t* N * X * F(X) *\t"<<endl;
+              cout<<"**********************************"<<endl;
+              cout<<"+----------+----------+----------+\n"<<endl;
+           for(i = 0; i<=x2; i++)
+          {
+            x3=x1*2;
+            //my function F(x)=x1*2
+            Dump();
+            x1=x1+N;
+          }
+        }
+    }
+  protected:
+    int x1,x2,N,var;
+    int x3,i;
+};
+
+
+int main()
+{
+     int x1,x2,x3,N,var;
+     setlocale(LC_ALL, "Russian");
+
+     MyFunc Func;
+     Func.CinArguments();
+     Func.Get();
+     Func.Calc();
+
+
+    return 0;
+}
